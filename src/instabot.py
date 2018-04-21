ba4479a1c7e8814041dacbeb0eb566e4a6fb9f41
velivelo <38439899+velivelo@users.noreply.github.com>
2018-04-21 12:14:25 +0200

    Update instabot.py

--- src/instabot.py
+++ src/instabot.py
@@ -18,6 +18,7 @@ class InstaBot ():
         "follow": "https://www.instagram.com/web/friendships/{}/follow/", # user_id
         "unfollow": "https://www.instagram.com/web/friendships/{}/unfollow/", # user_id
         "user_details": "https://www.instagram.com/{}/", # username
+        "api_user_details": "https://i.instagram.com/api/v1/users/{}/info/", # user_id
         "explore": "https://www.instagram.com/explore/tags/{}/?__a=1", # tag
         "like": "https://www.instagram.com/web/likes/{}/like/", # media_id
         "unlike": "https://www.instagram.com/web/likes/{}/unlike/", # media_id
@@ -53,14 +54,14 @@ class InstaBot ():
             'User-Agent': UserAgent().random,
         })
     
-        r = self.s.get(self.urls["default"])
+        r = self.s.get(self.urls["default"], timeout= 5)
         self.s.headers.update({'X-CSRFToken': r.cookies['csrftoken']})
-        time.sleep(2 * random.random())
+        time.sleep(random.uniform(1, 2))
 
         r = self.s.post(self.urls["login"], data= {
                                                 "username": self.username,
                                                 "password": self.password,
-                                            })
+                                            }, timeout= 5)
         self.s.headers.update({'X-CSRFToken': r.cookies['csrftoken']})
         self.s.cookies['ig_vw'] = '1536'
         self.s.cookies['ig_pr'] = '1.25'
@@ -68,9 +69,9 @@ class InstaBot ():
         self.s.cookies['ig_or'] = 'landscape-primary'
         if r.status_code != 200:
             raise Exception("Login error. Connection Error.")
-        time.sleep(2 * random.random())
+        time.sleep(random.uniform(1, 2))
 
-        r = self.s.get(self.urls["default"])
+        r = self.s.get(self.urls["default"], timeout= 5)
         if r.text.find(self.username) < 0:
             raise Exception("Login error. Check login data.")
         self.logged = True
@@ -78,81 +79,73 @@ class InstaBot ():
     @require_loggin
     def follow (self, user_id):
         """Follow an user by his id"""
-        r = self.s.post(self.urls["follow"].format(user_id))
-        if r.status_code != 200:
-            raise Exception('User id : "{}" is attribuate to no one'.format(user_id))
+        r = self.s.post(self.urls["follow"].format(user_id), timeout= 5)
+        r.raise_for_status()
         return r
 
     @require_loggin
     def unfollow (self, user_id):
         """Unfollow an user by his id"""
-        r = self.s.post(self.urls["unfollow"].format(user_id))
-        if r.status_code != 200:
-            raise Exception('User id : "{}" is attribuate to no one'.format(user_id))
+        r = self.s.post(self.urls["unfollow"].format(user_id), timeout= 5)
+        r.raise_for_status()
         return r
 
     @require_loggin
     def like (self, media_id):
         """Like a media by its id"""
-        r = self.s.post(self.urls["like"].format(media_id))
-        if r.status_code != 200:
-            raise Exception("{} is not a valid media id".format(media_id))
+        r = self.s.post(self.urls["like"].format(media_id), timeout= 5)
+        r.raise_for_status()
     
     @require_loggin
     def unlike (self, media_id):
         """Unlike a media by its id"""
-        r = self.s.post(self.urls["unlike"].format(media_id))
-        if r.status_code != 200:
-            raise Exception("{} is not a valid media id".format(media_id))
+        r = self.s.post(self.urls["unlike"].format(media_id), timeout= 5)
+        r.raise_for_status()
 
     @require_loggin
     def comment (self, media_id, text):
         """Post a comment on a media by its id and return the comment id"""
         r = self.s.post(self.urls["comment"].format(media_id), data= {
                                                                    "comment_text": text
-                                                               })
-        if r.status_code != 200:
-            raise Exception('Media with id: "{}" does\'nt exist')
+                                                               }, timeout= 5)
+        r.raise_for_status()
         return r.json()["id"]
 
     @require_loggin
     def removeComment (self, media_id, comment_id):
         """Remove a comment from a media by its id"""
-        r = self.s.post(self.urls["remove_comment"].format(media_id, comment_id))
-        if r.status_code != 200:
-            raise Exception('Media with id: "{}" or/and comment with id: "{}" don\'t exist')
+        r = self.s.post(self.urls["remove_comment"].format(media_id, comment_id), timeout= 5)
+        r.raise_for_status()
 
     def getUserDetails (self, username):
         """Return an user details into a dictionary from his username"""
-        r = self.s.get(self.urls["user_details"].format(username))
-        if r.status_code != 200:
-            raise Exception('"{}" is an unassigned username'.format(username))
+        r = self.s.get(self.urls["user_details"].format(username), timeout= 5)
+        r.raise_for_status()
         full_details = json.loads(re.search(r"window\._sharedData = (.+);</script>", r.text).group(1))
         return full_details["entry_data"]["ProfilePage"][0]["graphql"]["user"]
 
     def usernameToUserId (self, username):
         """From an username to an user id"""
         return self.getUserDetails(username)["id"]
-
-    @require_loggin    
+    
     def userIdToUsername (self, user_id):
         """From an user id to an username"""
-        r = self.s.get(self.urls["follow"].format(user_id))
-        return re.search(r".com/(.+)/", r.url).group(1)
+        r = requests.get(self.urls["api_user_details"].format(user_id), timeout= 5)
+        return r.json()["user"]["username"]
 
     def explore (self, tag):
         """Return the most recent posted medias containing the tag in argument"""
-        r = self.s.get(self.urls["explore"].format(tag))
+        r = self.s.get(self.urls["explore"].format(tag), timeout= 5)
         if r.status_code != 200: 
             return [] # no madia with this tag
         return [media["node"] for media in r.json()["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"]]
 
     def downloadMedia (self, media_shortcode):
         """Download a media by its shortcode (not videos)"""
-        r = self.s.get(self.urls["media"].format(media_shortcode))
+        r = self.s.get(self.urls["media"].format(media_shortcode), timeout= 5)
         full_details = json.loads(re.search(r"window\._sharedData = (.+);</script>", r.text).group(1))
         media_url = full_details["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["display_url"]
-        img_data = requests.get(media_url).content
+        img_data = requests.get(media_url, timeout= 10).content
         os.makedirs("downloads", exist_ok= True)
         with open("downloads/{}.jpg".format(media_shortcode), "wb") as handler:
             handler.write(img_data)
